fbf59c52c4526a5b76bde54ec55c5780eac43467
rocky <tak.koroki@gmail.com>
2013-06-05 15:39:19 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aitel::Application.config.secret_token = '46a5b26ac02b335012aaeeab78c857ad3069bf493e15f23417c37f005535190bbd09b95e1822b6c4e3a10961e1945da006c8244b062a341a0df12c15514768cb'
