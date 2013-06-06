7d17ff4503fe529feb7d403d91dabd8bcceb93c9
kouroki takanori <kouroki@aisel.ne.jp>
2013-06-06 16:22:04 +0900

    メッセージ送信基本処理を追加

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,22 @@
+class MessagesController < ShopBase
+
+  def new 
+    @message = Message.new
+    @vips = @current_shop.vips.includes(:user)
+  end
+
+  def create
+    puts params
+
+    to_users = params[:to_users] || []
+    to_users.map(&:to_i).each do |to_user|
+      message = Message.new( params[:message] )
+      message.shop_id = @current_shop.id
+      message.user_id = to_user
+      message.save!
+    end
+    
+    redirect_to new_message_path
+
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
+#coding: utf-8
 class User < ActiveRecord::Base
   has_many :vips
   has_many :shops , through: :vips
+  has_many :messages
   attr_accessible :email, :name, :password_digest
+
+  class << self
+    def search( condition , rel)
+      return rel if condition.nil?
+puts condition
+puts condition.name
+      return_rel = rel
+      if condition.name.present?
+        return_rel = return_rel.where( name: condition.name)
+      end
+      return return_rel
+    end
+  end
 end
