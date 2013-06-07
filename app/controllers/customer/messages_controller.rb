625fb47a33b0087d246427821ea260526a4b84c1
satou <satou@satou-VirtualBox.(none)>
2013-06-07 14:49:12 +0900

    issue#18の指摘対応

--- app/controllers/customer/messages_controller.rb
+++ app/controllers/customer/messages_controller.rb
@@ -1,7 +1,7 @@
 class Customer::MessagesController < Customer::CustomerBase
   
   def index
-    @messages = Message.sent_to(@current_user).order("created_at DESC")
+    @messages = Message.includes(:shop).sent_to(@current_user).order("created_at DESC")
   end
   
   def show
