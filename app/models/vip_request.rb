899ebf9a24629eb79d770cc6fec167d0819ef732
satou <satou@satou-VirtualBox.(none)>
2013-06-11 10:23:49 +0900

    お客側：VIPの招待を承認、拒否できる

--- app/models/vip_request.rb
+++ app/models/vip_request.rb
@@ -5,7 +5,11 @@ class VipRequest < ActiveRecord::Base
 
   scope :by, ->(shop){ where( shop_id: shop.id) }
   scope :requesting, ->{ where( status: STATUS_INVITING) }
+  scope :sent_to, ->( user ){ where( user_id: user.id  ) }
+
 
   STATUS_INVITING = 0
+  STATUS_APPROVE = 1
+  STATUS_REJECT = 2
 
 end
