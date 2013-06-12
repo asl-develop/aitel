ae38cb6a0d960e145287be8c53894f5ad6eeaaab
kouroki <kouroki@aisel.ne.jp>
2013-06-12 15:17:17 +0900

    User,Shopに属する関係をmodule化

--- app/models/arrival_log.rb
+++ app/models/arrival_log.rb
@@ -1,5 +1,4 @@
 class ArrivalLog < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :shop
-  attr_accessible :arrival_time, :user_id, :shop_id
+  include VipAssosiation
+  attr_accessible :arrival_time 
 end
