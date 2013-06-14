c9640d5f2f53e0e65ace578191ebe453d2ee826d
kouroki <kouroki@aisel.ne.jp>
2013-06-14 16:31:56 +0900

    もういちどheroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Aitel::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( shop_base.css )
+  config.assets.precompile += %w( shop_side_base.css )
   config.assets.precompile += %w( customer_base.css )
 
   # Disable delivery errors, bad email addresses will be ignored
