0f068f733286751896cdfb52130f4352d4bf0547
kouroki <kouroki@aisel.ne.jp>
2013-06-17 08:25:05 +0900

    スタイル調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,33 @@
+#coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-user = FactoryGirl.create( :user, email: 'test_user@test.com', password_digest: 'password')
+user_ids = [ 
+  FactoryGirl.create( :user, email: 'test_user1@test.com', password_digest: 'password'),
+  FactoryGirl.create( :user, email: 'test_user2@test.com', password_digest: 'password'),
+  FactoryGirl.create( :user, email: 'test_user3@test.com', password_digest: 'password'),
+  FactoryGirl.create( :user, email: 'test_user4@test.com', password_digest: 'password'),
+  FactoryGirl.create( :user, email: 'test_user5@test.com', password_digest: 'password')
+].map(&:id)
 
-#10.times { FactoryGirl.create( :user_with_want )}
+shop_ids = [ 
+  FactoryGirl.create( :shop, login_id: "test_shop1", name: "料亭AISEL", email: 'test_shop1@test.com', password_digest: 'password'),
+  FactoryGirl.create( :shop, login_id: "test_shop2", name: "居酒屋AISEL", email: 'test_shop2@test.com', password_digest: 'password'),
+  FactoryGirl.create( :shop, login_id: "test_shop3", name: "AISEL-BAR", email: 'test_shop3@test.com', password_digest: 'password'),
+  FactoryGirl.create( :shop, login_id: "test_shop4", name: "AISELレストラン", email: 'test_shop4@test.com', password_digest: 'password'),
+  FactoryGirl.create( :shop, login_id: "test_shop5", name: "AISEL亭", email: 'test_shop5@test.com', password_digest: 'password')
+].map(&:id)
 
-shop = FactoryGirl.create( :shop, login_id: "test_shop", name: "AISEL", email: 'test_shop@test.com', password_digest: 'password')
 
-FactoryGirl.create( :vip, user_id: user.id, shop_id: shop.id, property: 'password')
+FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[0], property: 'サッカーが好き。日本酒が好き。')
+FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[1], property: '')
+FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[2], property: '')
+FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[3], property: '')
+FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[4], property: '')
 
-
+FactoryGirl.create( :vip, user_id: user_ids[1], shop_id: shop_ids[0], property: '')
+FactoryGirl.create( :vip, user_id: user_ids[2], shop_id: shop_ids[0], property: '')
+FactoryGirl.create( :vip, user_id: user_ids[3], shop_id: shop_ids[0], property: '')
+FactoryGirl.create( :vip, user_id: user_ids[4], shop_id: shop_ids[0], property: '')
