878b8b687b0035ae32340c951019ddb836ca5d3a
kouroki <kouroki@aisel.ne.jp>
2013-06-17 15:45:00 +0900

    スタイル

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@
 # Examples:
 #
 user_ids = [ 
-  FactoryGirl.create( :user, email: 'test_user1@test.com', password_digest: 'password'),
-  FactoryGirl.create( :user, email: 'test_user2@test.com', password_digest: 'password'),
-  FactoryGirl.create( :user, email: 'test_user3@test.com', password_digest: 'password'),
-  FactoryGirl.create( :user, email: 'test_user4@test.com', password_digest: 'password'),
-  FactoryGirl.create( :user, email: 'test_user5@test.com', password_digest: 'password')
+  FactoryGirl.create( :user, name: 'アイセル 太郎', email: 'test_user1@test.com', password_digest: 'password'),
+  FactoryGirl.create( :user, name: '鈴木 アイセル', email: 'test_user2@test.com', password_digest: 'password'),
+  FactoryGirl.create( :user, name: '田中 ＡＳＬ', email: 'test_user3@test.com', password_digest: 'password'),
+  FactoryGirl.create( :user, name: '佐藤 アイセル', email: 'test_user4@test.com', password_digest: 'password'),
+  FactoryGirl.create( :user, name: '高橋 アイセル', email: 'test_user5@test.com', password_digest: 'password')
 ].map(&:id)
 
 shop_ids = [ 
@@ -20,14 +20,26 @@ shop_ids = [
   FactoryGirl.create( :shop, login_id: "test_shop5", name: "AISEL亭", email: 'test_shop5@test.com', password_digest: 'password')
 ].map(&:id)
 
-
 FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[0], property: 'サッカーが好き。日本酒が好き。')
-FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[1], property: '')
-FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[2], property: '')
-FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[3], property: '')
-FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[4], property: '')
+FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[1], property: '野球が好き。')
+FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[2], property: 'スポーツ全般好き。サッカーが得に好き。')
+FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[3], property: '日本酒が好き')
+FactoryGirl.create( :vip, user_id: user_ids[0], shop_id: shop_ids[4], property: 'ワインが好き')
 
 FactoryGirl.create( :vip, user_id: user_ids[1], shop_id: shop_ids[0], property: '')
 FactoryGirl.create( :vip, user_id: user_ids[2], shop_id: shop_ids[0], property: '')
 FactoryGirl.create( :vip, user_id: user_ids[3], shop_id: shop_ids[0], property: '')
 FactoryGirl.create( :vip, user_id: user_ids[4], shop_id: shop_ids[0], property: '')
+
+main_user = User.find( user_ids[0] )
+main_shop = Shop.find( shop_ids[0] )
+arrival_log = main_user.arrival_logs.build
+arrival_log.arrival_time = Time.local(2001, 5, 20, 23, 59, 59)
+arrival_log.shop_id = main_shop.id
+arrival_log.save
+
+
+
+
+
+
